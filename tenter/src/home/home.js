f2b7cdaa765b8094afd5f5eaefc24a71d376d227
bobacrack <92kuni1bif@hft-stuttgart.de>
2023-05-11 10:03:06 +0200

    home improvements

--- tenter/src/home/home.js
+++ tenter/src/home/home.js
@@ -1,7 +1,70 @@
-function Home() {
-    return (
-        <p></p>
-    )
-}
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import logo from './IMG_0180.jpg'
+import { SearchOutlined, CloseOutlined } from '@ant-design/icons';
+import { Button, Tooltip, Space } from 'antd';
 
-export default Home
+const TinderCard = () => {
+    const [images, setImages] = useState([]);
+    const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+    useEffect(() => {
+        fetchImages();
+    }, []);
+
+    const fetchImages = async () => {
+        try {
+            const response = await axios.get('https://example.com/images'); // Anpassen der URL entsprechend deiner Backend-Implementierung
+            setImages(response.data);
+        } catch (error) {
+            console.error('Error fetching images:', error);
+        }
+    };
+
+    const handleSwipeLeft = () => {
+        if (currentImageIndex < images.length - 1) {
+            setCurrentImageIndex(currentImageIndex + 1);
+        }
+    };
+
+    const handleSwipeRight = () => {
+        if (currentImageIndex > 0) {
+            setCurrentImageIndex(currentImageIndex - 1);
+        }
+    };
+
+    return (
+        <div className="tinder-card">
+            <div className="image-container">
+                {images.length > 0 && (
+                    //<img src={images[currentImageIndex]} alt="Tinder Card" />
+                    <img src={logo} alt="Tinder Card" />
+                )}
+
+            </div>
+            <div className="button-container">
+                <Space direction="vertical">
+                    <Space wrap>
+                        <Tooltip title="search">
+                            <Button type="primary" style={{ height: 50, width: 50 }} shape="circle" icon={<SearchOutlined />} />
+                        </Tooltip>
+                        <Tooltip title="search">
+                            <Button type="info" style={{ height: 50, width: 50 }} shape="circle" icon={<CloseOutlined />} />
+                        </Tooltip>
+                        <Tooltip title="search">
+                            <Button type="primary" style={{ height: 50, width: 50 }} shape="circle" icon={<SearchOutlined />} />
+                        </Tooltip>
+                        <Tooltip title="search">
+                            <Button type="primary" style={{ height: 50, width: 50 }} shape="circle" icon={<SearchOutlined />} />
+                        </Tooltip>
+                        <Tooltip title="search">
+                            <Button type="primary" style={{ height: 50, width: 50 }} shape="circle" icon={<SearchOutlined />} />
+                        </Tooltip>
+                    </Space>
+                </Space>
+            </div>
+        </div>
+    );
+};
+
+export default TinderCard;
