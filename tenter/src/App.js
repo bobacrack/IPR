0be2dc7486301be8d7e0b8cc257c51b214848aca
1 <a1@1s-macbook-air.speedport.ip>
2023-06-11 22:25:51 +0200

    updated Chat

--- tenter/src/App.js
+++ tenter/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './header/Header';
 import Card from './card/Card';
 import Chats from './Chats';
+import ChatScreen from './ChatScreen';
 import SwipeButtons from './SwipeButtons';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -45,6 +46,7 @@ function App() {
   <div className="App">
     <Router>
       <Routes>
+      <Route path="/chats/:tents" element={<div><Header backButton="/chats" /><ChatScreen /></div>} />
         <Route path="/chats" element={<div><Header backButton="/" /><Chats /></div>} />
         <Route path="/" element={<div><Header/><Card /><SwipeButtons /></div>} />
       </Routes>
--- tenter/src/Chat.js
+++ tenter/src/Chat.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import  Avatar  from '@material-ui/core/Avatar';
+import { Link } from 'react-router-dom';
+import "./Chat.css";
 
 
 function Chat({name, message, profilePic, timestamp}){
     return (
+    <Link to={'/chats/${name}'}>
     <div className="chat">
-        <Avatar className="chat__image" alt={name} src={profilePic} />
-        <div>
+        <Avatar className="chat__image" src={profilePic} />
+        <div className="chat__details">
             <h2>{name}</h2>
             <p>{message}</p>
         </div>
-        <p>{timestamp}</p>
+        <p className="chat__timestamp">{timestamp}</p>
     </div>
+    </Link>
+    
     );
 }
 
