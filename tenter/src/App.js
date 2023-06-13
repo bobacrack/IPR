0bccaec34bd3b1cc579c0886e6c215a4ba40526a
1 <a1@1s-macbook-air.speedport.ip>
2023-06-14 01:28:40 +0200

    edit

--- tenter/src/App.js
+++ tenter/src/App.js
@@ -35,59 +35,15 @@ function App() {
 
 
   return (
-<<<<<<< HEAD
-    /*
-     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/chat">
-            <Header backButton="/" />
-            <Chats />
-          </Route>
-          <Route path="/">
-            <Header />
-            <Cards />
-            <SwipeButtons />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-
-    
-
-
-    <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path='/chats'>
-          
-          </Route>
-          <Route path="/" element={<div><Card /><SwipeButtons /></div>} />
-        </Routes>
-      </Router>
-<Route path="/" element={<div><Header /><Card /><SwipeButtons /></div>} />
-    </div >
-  */
-    <div className="App">
-=======
     < div className="App" >
->>>>>>> 6386c72ab06cda873be64cca5ce7168360209380
       <Router>
         <Routes>
           <Route path="/chats/:tents" element={<div><Header backButton="/chats" /><ChatScreen /></div>} />
           <Route path="/chats" element={<div><Header backButton="/" /><Chats /></div>} />
-<<<<<<< HEAD
           <Route path="/" element={<div><Header /><Card /></div>} />
-          <Route path='/login' element={<div><Header /> <RegistrationPage /></div>} />
-          <Route path='/signIn' element={<div><Header /> <SignUp /><SignIn /></div>} />
-
-=======
-          <Route path="/" element={<div><Header /><Card /><SwipeButtons /></div>} />
           <Route path='/register' element={<div><Header /> <RegistrationPage /></div>} />
           <Route path='/login' element={<div><Header /> <LogIn /></div>} />
           <Route path='/profile/:uid' element={<div><Header /> <Profile /></div>} />
->>>>>>> 6386c72ab06cda873be64cca5ce7168360209380
         </Routes>
       </Router>
     </div >
