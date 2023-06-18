5f137ad0ba92d8c2979eba45cb68d08f9d1e542a
lluebbe <92lule1bif@hft-stuttgart.de>
2023-06-18 16:44:37 +0200

    updated strutct file

--- internal/structs/structs.go
+++ internal/structs/structs.go
@@ -1,6 +1,23 @@
 package structs
 
 type User struct {
+	ID        int
+	UID       int
 	Firstname string
 	Lastname  string
+	Age       *time.Time
+	AgePref   int
+	Picture   string
+}
+type Chat struct {
+	ID          int
+	UIDSender   int
+	UIDReceiver int
+	Message     string
+	Timestamp   *time.Time
+}
+type Like struct {
+	ID       int
+	UIDLiker int
+	UIDLiked int
 }
