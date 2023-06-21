445477bc14c3fdb3480a325ac9262607b2a4b90b
lluebbe <92lule1bif@hft-stuttgart.de>
2023-06-21 13:21:14 +0200

    realtime Chat

--- tenter/src/ChatScreen.js
+++ tenter/src/ChatScreen.js
@@ -5,6 +5,8 @@ import { database, auth } from './firebase';
 import { collection, addDoc, serverTimestamp, doc, getDoc, query, where, getDocs, onSnapshot } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
 import { useParams } from 'react-router-dom';
+import { fetchChats } from './card/fetchChats';
+import { fetchUsers } from './card/fetchUsers';
 
 
 
@@ -12,18 +14,38 @@ import { useParams } from 'react-router-dom';
 function ChatScreen() {
     const { receiverInfo } = useParams();
     const [input, setInput] = useState('');
-    const [messages, setMessages] = useState([]);
+    // const [messages, setMessages] = useState([]);
     const [userID, setUserID] = useState(null);
 
 
+    const [usersData, setusersData] = useState([]);
+
+    const [chatsData, setchatsData] = useState([]);
+    //const uidOfCurrentUser = auth.currentUser.uid;
+    console.log("Input" + input);
+    // console.log("Message: ", messages)
+
     useEffect(() => {
-        const fetchData = async () => {
-            const querySnapshot = await getDocs(collection(database, "chats"));
-            //const q = query(collection(database, 'chats'), where('recevierId', '==', String(userID)));
-            //const querySnapshot = await getDoc(q)
-            const tempData = querySnapshot.docs.map((doc) => doc.data());
-            setMessages(tempData.sort((a, b) => a.timestamp - b.timestamp));
-        };
+        fetchUsers((data, error) => {
+            if (data) {
+                // Save the fetched users in the usersData state
+                //console.log("DATA Result USER: ", data);
+                setusersData(data);
+                //console.log(usersData);
+            } else {
+                console.error(error);
+            }
+        });
+        fetchChats((data, error) => {
+            if (data) {
+                // Save the fetched users in the usersData state
+                console.log("DATA Result: ", data);
+                setchatsData(data);
+                //console.log(usersData);
+            } else {
+                console.error(error);
+            }
+        });
 
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
@@ -33,59 +55,76 @@ function ChatScreen() {
             }
         });
 
-        fetchData();
-
-        const unsubscribeRealtime = onSnapshot(collection(database, "chats"), (snapshot) => {
-            const updatedData = snapshot.docs.map((doc) => doc.data());
-            setMessages(updatedData.sort((a, b) => a.timestamp - b.timestamp));
-        });
-
         return () => {
             unsubscribe();
-            unsubscribeRealtime();
-        };
+        }
     }, []);
 
+    function findMatchingId(userID, usersData) {
+        const matchingUser = usersData.find(user => user.uid === userID);
+        return matchingUser ? matchingUser.id : null;
+    }
+    const matchingUserId = findMatchingId(userID, usersData);
+    const matchingReceiverId = findMatchingId(receiverInfo, usersData);
+
+    console.log("SENDERID: " + matchingUserId);
+    console.log("ReceiverID: " + matchingReceiverId);
+
     const handleSend = async (e) => {
-        e.preventDefault();
-
         const newMessage = {
-            message: input,
-            recevierId: receiverInfo,
-            senderId: userID,
-            timestamp: serverTimestamp()
+            uidsender: matchingUserId,
+            uidreceiver: matchingReceiverId,
+            message: input
         };
-
+        console.log("Message: " + newMessage);
         try {
-            // Daten in der Firestore Collection "messages" speichern
-            const docRef = await addDoc(collection(database, "chats"), newMessage);
+            // POST-Anfrage an deine API-Endpunkt senden, um die Daten in der Datenbank zu speichern
+            const response = await fetch("http://localhost:6969/api/v1/chat", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(newMessage)
+            });
+
+
+            if (response.ok) {
+                console.log("Nachricht erfolgreich gesendet");
+            } else {
+                console.error("Fehler beim Senden der Nachricht:", response.status);
+            }
         } catch (error) {
             console.error("Fehler beim Senden der Nachricht:", error);
+            console.log("TESTTST: ", chatsData);
+            console.log("NEWWW", newMessage);
         }
 
-        setMessages([...messages, newMessage]);
-        setInput("")
+        //setchatsData([...chatsData, newMessage]);
+        setInput("");
     };
 
     const date = new Date().toDateString()
+    console.log("MESSSAGESS: ", chatsData);
 
-    // Beispielaufruf
 
 
     return (
         <div className="chatScreen">
             <p className="chatScreen__timestamp">YOU MATCHED WITH ELLEN ON {date}</p>
-            {messages.map((message, index) => (
-                (String(message.senderId) === String(userID) && String(message.recevierId) === String(receiverInfo)) ? (
+            {chatsData.map((message, index) => (
+
+                (message.uidsender === matchingUserId && message.uidreceiver === matchingReceiverId) ? (
+
                     <div key={index} className="chatScreen__message">
                         <p className="chatScreen__textUser">{message.message}</p>
                     </div>
-                ) : (String(message.senderId) !== String(userID) && String(message.senderId) === String(receiverInfo)) ? (
+                ) : (message.uidsender !== matchingUserId && message.uidsender === matchingReceiverId) ? (
                     <div key={index} className="chatScreen__message">
-                        <Avatar className="chatScreen__image" alt={message.recevierIds} src={message.image} />
+                        <Avatar className="chatScreen__image" alt={String(message.uidreceiver)} src={usersData.find(user => user.id === matchingReceiverId)?.picture} />
                         <p className="chatScreen__text">{message.message}</p>
                     </div>
                 ) : null
+
             ))}
 
             <form className="chatScreen__input">
